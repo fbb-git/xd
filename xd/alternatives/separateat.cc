dc05ce434018d389f85446fca9499a7886b6f004
Frank B. Brokken <f.b.brokken@rug.nl>
2010-12-12 15:47:44 +0000

    wip
    git-svn-id: https://xd-home.svn.sourceforge.net/svnroot/xd-home/xd/trunk@810 dcc949cb-2a1c-0410-b67a-e7e5ca830264

---
+++ xd/alternatives/separateat.cc
@@ -0,0 +1,13 @@
+#include "alternatives.ih"
+
+size_t Alternatives::separateAt() const
+{
+    if (not d_separate)
+        return UINT_MAX;
+
+    return 
+        d_history.position() == History::TOP ?
+            d_nInHistory
+        :
+            size() - d_nInHistory;
+}
